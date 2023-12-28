fc925b3e6ca512113050c3c045dd2c63592f10c8
thpig0905 <kth090567@gmail.com>
2023-12-28 15:39:08 +0900

    ver1.1
    write main of package Main class.
    still not worked and I still crying.

--- src/Cotroller/Main.java
+++ src/Cotroller/Main.java
@@ -1,4 +1,8 @@
 package Cotroller;
 
 public class Main {
+    public static void main(String[] args) {
+        MallController mallController = MallController.getInstance();
+        mallController.init();
+    }
 }
